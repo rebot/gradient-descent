740f767bc63a3791c13e33d75736bcf6ae72c103
B0hr <trenson.gilles@gmail.com>
2017-06-20 12:10:03 +0200

    Bugfix

--- run.py
+++ run.py
@@ -2,36 +2,26 @@ import os
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from IPython.display import HTML
-
-Writer = animation.writers['ffmpeg']
-writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
 
 x = np.linspace(1, 20, 30)
 y = np.linspace(3, 50, 30) + 5
 
-distances = np.vectorize(lambda x, y: (x - y) ** 2, otypes=[np.float])
-distance = lambda x,y: np.sum(distances(x,y)) / float(len(x))
+error = lambda x,y: np.mean(np.vectorize(lambda x, y: (x - y) ** 2, otypes=[np.float64]))
 
-afg_b = np.vectorize(lambda x, y, b, m: 2 * ((b * x + m) - y) * (x))
-afg_m = np.vectorize(lambda x, y, b, m: 2 * ((b * x + m) - y) * (1))
+grad_m = np.vectorize(lambda x, y, m, b: 2 * ((m * x + b) - y) * (x), otypes=[np.float64])
+grad_b = np.vectorize(lambda x, y, m, b: 2 * ((m * x + b) - y) * (1), otypes=[np.float64])
 
 def gradient_decent_runner(points, initital_b, initial_m, learning_rate, num_iterations):
-    fig = plt.figure()
-    plots = []
     x,y = points
     p = np.array([initital_b,initial_m])
     for _ in range(num_iterations):
-        error = distance(y, np.polyval(p, x))
         p = np.array([
-            p[0] + learning_rate * (1 if np.sum(afg_b(x,y,p[0],p[1])) < 0 else -1),
-            p[0] + learning_rate * (1 if np.sum(afg_m(x,y,p[0],p[1])) < 0 else -1)
+            p[0] + learning_rate * np.mean(grad_m(x,y,p[0],p[1])),
+            p[0] + learning_rate * np.mean(grad_b(x,y,p[0],p[1]))
         ])
-        plots.append((plt.scatter(x,np.polyval(p, x)),))
-    anim = animation.ArtistAnimation(fig, plots, interval=50, repeat_delay=3000, blit=True)
-    anim.save('anim.mp4', writer=writer)
+    plt.plot(x,y,'r',x,np.polyval(p, x),'g')
+    plt.show()
     return p
 
 if __name__ == '__main__':
-    [b, m] = gradient_decent_runner([x,y],0,0,0.005,50)
+    [b, m] = gradient_decent_runner([x,y],0,0,0.01,1000)
